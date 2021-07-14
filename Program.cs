cba6c7807367e76b0b7fb0623d23f81dc3a300f3
= <lopke123@live.nl>
2021-07-14 13:54:30 +0200

    fixed error

--- Program.cs
+++ Program.cs
@@ -12,34 +12,7 @@ namespace Qserver
     {
         static void Main(string[] args)
         {
-            CraneRepository crane = new CraneRepository(DatabaseManager.MySqlFactory);
-            //var items = crane.GetCraneItems().Result;
-
-
-            LaunchServer();
-            while (true)
-            {
-                Console.Write("- [1]: Launch Server\nSelect option: ");
-                var key = Console.ReadKey();
-                Console.WriteLine();
-
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.D1:
-                        LaunchServer();
-                        break;
-                    default:
-                        Console.WriteLine("Option invalied!");
-                        break;
-                }
-            }
-                
-        }
-
-        private static void LaunchServer()
-        {
-            GameServer.GameServer.Start();
+            GameServer.GameServer.Start(args);
         }
     }
 }

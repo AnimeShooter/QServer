21628ce915649cde0f221aa9765ae9932087cd06
= <lopke123@live.nl>
2021-07-15 18:14:49 +0200

    update LobbyManager Account

--- GameServer/Network/Managers/LobbyManager.cs
+++ GameServer/Network/Managers/LobbyManager.cs
@@ -122,12 +122,90 @@ namespace Qserver.GameServer.Network.Managers
             return pw;
         }
 
-        public PacketWriter UpdateAcount()
+        public PacketWriter UpdateAcount(Player player)
         {
             PacketWriter pw = new PacketWriter(Opcode.LOBBY_UPDATE_ACCOUNT);
+            pw.WriteUInt32(player.PlayerId);
+            pw.WriteBytes(new byte[42]); // OG 42
+            pw.WriteWString(player.Name, 16);
+            pw.WriteUInt32(player.StatsManager.PlayTimeInMinutes);
+            pw.WriteUInt32(player.Cash);
+            pw.WriteUInt32(player.Rank);
+            pw.WriteUInt16(0); // unk
+            pw.WriteUInt16(player.Character);
+            pw.WriteUInt32(player.Level);
+            pw.WriteBytes(new byte[8]);
 
-            // TODO
-            throw new NotImplementedException();
+            // player settings
+            pw.WriteUInt8(1); // accept PM
+            pw.WriteUInt8(1); // accept game invites 
+            pw.WriteUInt8(1); // accept friend invites 
+            pw.WriteUInt8(1); // unk
+            pw.WriteUInt8(1); // accept trade requests
+            pw.WriteBytes(new byte[20]);
+            pw.WriteUInt8(4); // tutorial status (4)
+            pw.WriteBytes(new byte[12]);
+
+            // player containers
+            pw.WriteUInt16(200); // max inventory size
+            pw.WriteUInt16(50); // max friendlist size
+            pw.WriteUInt16(10); // max inc/out friend list size
+            pw.WriteUInt16(50); // max memo send count
+            pw.WriteUInt16(50); // left over memo send count
+
+            // player stats
+            pw.WriteUInt32(1); // ?
+            pw.WriteUInt32(player.Experience);
+            pw.WriteUInt32(player.Don);
+            pw.WriteUInt32(0);
+            pw.WriteUInt32(player.StatsManager.Kills);
+            pw.WriteUInt32(player.StatsManager.Deaths);
+            pw.WriteUInt32(player.StatsManager.NormalWins);
+            pw.WriteUInt32(player.StatsManager.NormalLosses);
+            pw.WriteUInt32(player.StatsManager.NormalDrews);
+            pw.WriteUInt32(0); // unk
+            pw.WriteUInt32(player.StatsManager.MissionWins);
+            pw.WriteUInt32(player.StatsManager.MissionLosses);
+            pw.WriteUInt32(player.StatsManager.MissionDrews);
+
+            pw.WriteBytes(new byte[72]); // unk
+
+            pw.WriteUInt32(player.StatsManager.SlackerPoints);
+            pw.WriteUInt32(player.Coins);
+            pw.WriteBytes(new byte[44]);
+
+            var characters = player.EquipmentManager.UnlockerCharacters;
+            foreach (ushort character in characters)
+            {
+                //ulong[] armor = new ulong[9]; // player.EquipmentManager.GetArmorByCharacter(character); // 9
+                //ulong[] weapons = new ulong[4]; // player.EquipmentManager.GetWeaponsByCharacter(character); // 4
+
+                ulong[] armor = player.EquipmentManager.GetArmorByCharacter(character); // 9
+                ulong[] weapons = player.EquipmentManager.GetWeaponsByCharacter(character); // 4
+
+                pw.WriteUInt16(character);
+                foreach (ulong armorCardId in armor)
+                    pw.WriteUInt64(armorCardId);
+
+                foreach (ulong weaponCardId in weapons)
+                {
+                    pw.WriteUInt64(weaponCardId);
+                    pw.WriteUInt64(0); // unk
+                }
+            }
+
+            pw.WriteBytes(new byte[2455 - (138 * characters.Count)]);
+
+
+            var achievements = player.AchievementContainer.List;
+            pw.WriteUInt16((ushort)achievements.Count);
+            foreach (var achievement in achievements)
+            {
+                pw.WriteUInt32(achievement);
+                pw.WriteUInt32(0); // unk
+            }
+
+            pw.WriteBytes(new byte[1635 - achievements.Count * 8]);
 
             return pw;
         }
